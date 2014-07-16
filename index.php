48f9cf2fa39a45861fa2fab7f2fb4b893258d3a0
Brett Chisholm <brett.chisholm@propcom.co.uk>
2014-07-16 17:27:51 +0100

    Fix missing js file error

--- index.php
+++ index.php
@@ -71,7 +71,7 @@
 		</div>
 
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-		<script type="text/javascript" src="main.js"></script>
+		<script type="text/javascript" src="tabbed-content-init.js"></script>
 		<script type="text/javascript" src="tabbed-content.js"></script>
 
 	</body>
